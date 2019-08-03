0a069a5f65222daa89312da72d663f9d9d9adab6
Daniel Kantor <home@Kantors-MacBook-Pro.local>
2019-08-03 19:19:19 -0400

    Fixed errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:session][:username])
         if user && user.authenticate(params[:session][:password])
-            user.status = true;
             session[:user_id] = user.id
             flash[:success] = "You have successfully logged in."
             redirect_to root_path
@@ -21,7 +20,6 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-        current_user.status = false;
         flash[:success] = "You have successfully logged out"
         redirect_to login_path
     end
